87ecdd1626b5e3d9d8134d5c947b6bb3a71e7c37
Unknown <58070114@kmitl.ac.th>
2017-08-23 17:47:31 +0700

    Send Homework and Lab02

--- lab02/MyApplication/app/src/main/java/myfirstapp/example/com/myapplication/MainActivity.java
+++ lab02/MyApplication/app/src/main/java/myfirstapp/example/com/myapplication/MainActivity.java
@@ -1,7 +1,7 @@
 package myfirstapp.example.com.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,11 +9,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        //setContentView(R.layout.activity_linearlayout_lab_01);
+        setContentView(R.layout.activity_linearlayout_lab_01);
         //setContentView(R.layout.activity_constrainlayout_lab_01);
         //setContentView(R.layout.activity_constrainlayout_lab_02);
         //setContentView(R.layout.activity_constrainlayout_lab_03);
-        setContentView(R.layout.homework_011);
+
 
     }
 }
